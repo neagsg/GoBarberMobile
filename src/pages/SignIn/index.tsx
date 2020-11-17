7ae7bdff9fda66c59bec236fa70600ee4e9a0362
Gean Gomes <geansg4@gmail.com>
2020-11-17 14:51:01 -0300

    add component button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Image } from 'react-native';
 
 import { Container, Title } from './styles';
+import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
     <Title>Faça seu logon</Title>
+    <Button onPress={() => alert('Entrar')}>Entrar</Button>
   </Container>
 );
 
