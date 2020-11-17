66f58a66694f4aae6cb72ea97693cd87464953ac
Gean Gomes <geansg4@gmail.com>
2020-11-17 15:31:23 -0300

    add component input

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Image } from 'react-native';
 
 import { Container, Title } from './styles';
-import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
     <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
     <Button onPress={() => alert('Entrar')}>Entrar</Button>
   </Container>
 );
