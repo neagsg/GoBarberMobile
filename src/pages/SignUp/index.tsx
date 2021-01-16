ee913659cc83a87e5c64d05a8234d4573414a7c2
Gean Gomes <geansg4@gmail.com>
2021-01-16 09:20:42 -0300

    add form configuration

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   Image,
   View,
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 import logoImg from '../../assets/logo.png';
@@ -15,6 +17,7 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
   return (
     <>
@@ -34,11 +37,19 @@ const SignUp: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Input name="user" icon="user" placeholder="Nome" />
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+            <Form
+              style={{ width: '100%' }}
+              ref={formRef}
+              onSubmit={data => console.log(data)}
+            >
+              <Input name="user" icon="user" placeholder="Nome" />
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
 
-            <Button onPress={() => {}}>Entrar</Button>
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
           </Container>
           <BackToSignIn onPress={() => navigation.navigate('SignIn')}>
             <Icon name="arrow-left" size={20} color="#fff" />
